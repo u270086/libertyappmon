82edd64e2060140ff8cbf97ddf9766a3a3bad720
Jason <jason.court25@outlook.com>
2026-05-21 13:00:46 +0100

    Add .gitignore to ignore build output

--- target/libertyappmon/js/dropdown.js
+++
@@ -1,9 +0,0 @@
-function toggleDownloadButton() {
-  const selector = document.getElementById("plexSelector");
-  const button = document.getElementById("downloadBtn");
-  button.style.display = selector.value === "ALL" ? "inline-block" : "none";
-}
-
-function downloadCSV() {
-  window.location.href = "DownloadServlet?plex=ALL";
-}
